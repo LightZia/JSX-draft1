833d7abe0fb37d51abf3c385adbe87fe4c378c00
Ansari Farah <ziaisme@outlook.com>
2025-11-15 02:51:02 +0530

    Initialized Project

---
+++ practice-project-1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    <div id="alive">
+      <h2>It's time for a revolution</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
