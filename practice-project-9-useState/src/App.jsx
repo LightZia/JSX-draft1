b9fbd89d57b55d26e7cd92b8de34df6b2f39f37b
Ansari Farah <ziaisme@outlook.com>
2026-02-19 16:17:25 +0530

    design change

--- practice-project-9-useState/src/App.jsx
+++ practice-project-9-useState/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   const [num, setNum] = useState(0)
 
   return (
-    <div>
+    <div className='overflow-hidden'>
       <div className = 'bg-[url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperaccess.com%2Ffull%2F6875927.jpg&f=1&nofb=1&ipt=08c3f09507fc0fa4b90205ee563b2f420477e71284dde7f009b2906f88984b78)] bg-cover h-screen w-screen flex justify-center items-center flex-col'>
         <div className='bg-white/2 backdrop-blur-md w-screen m-5 py-15 rounded-xl text-5xl text-white font-bold flex items-center justify-center'>
           <h1>{num}</h1>
