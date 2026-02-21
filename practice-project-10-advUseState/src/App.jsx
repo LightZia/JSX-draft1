ecbd57ca72a531322f54006f8d72f3c99445846f
Ansari Farah <ziaisme@outlook.com>
2026-02-21 13:03:15 +0530

    New Practice file

--- practice-project-10-advUseState/src/App.jsx
+++ practice-project-10-advUseState/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [obj, setObj] = useState({user:'Zia', age:18})
   const [arr, setArr] = useState([10, 20, 30])
   const [secObj, setSecObj] = useState({user:"Habibi", age:23})
+  const [num, setNum] = useState(10)
 
   const btnClicked = () => {
     const newObj = {...obj}
@@ -16,6 +17,10 @@ const App = () => {
 
     setSecObj(prev => ({...prev, age:20, user:'Habib'}))
 
+    setNum(prev => (prev+1))
+    setNum(prev => (prev+1))
+    setNum(prev => (prev+1))
+
     setObj(newObj)
     setArr(newArr)
   }
@@ -25,6 +30,7 @@ const App = () => {
       <h1>{obj.user} {obj.age}</h1>
       <h1>{arr}</h1>
       <h1>{secObj.user}, {secObj.age}</h1>
+      <h1>{num}</h1>
       <button onClick={btnClicked}>Click</button>
     </div>
   )
